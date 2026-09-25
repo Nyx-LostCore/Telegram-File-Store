d8dd7c3fa22b190e7bc5af98bd82db85012df5de
×͜× 「 ʟᴏꜱᴛ ᴇxɪꜱᴛᴇɴᴄᴇ 」 ×͜× <thebochan9@gmail.com>
2026-09-25 13:14:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ load_dotenv()
 from logging.handlers import RotatingFileHandler
 
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-APP_ID = int(os.environ.get("APP_ID", "0"))
-API_HASH = os.environ.get("API_HASH", "")
+APP_ID = int(os.environ.get("APP_ID", "36428426"))
+API_HASH = os.environ.get("API_HASH", "30cba30aa38699e77ce264365e327528")
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "0"))
-OWNER = os.environ.get("OWNER", "")
-OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
+OWNER = os.environ.get("OWNER", "akito_yamada_a")
+OWNER_ID = int(os.environ.get("OWNER_ID", "8663573756"))
 PORT = os.environ.get("PORT", "8001")
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://animekyoto:W5Itr6v2bZ3KpQF8@cluster0.trg5dtc.mongodb.net/?appName=Cluster0")
 
 if not TG_BOT_TOKEN:
     logging.warning("TG_BOT_TOKEN is not set!")
